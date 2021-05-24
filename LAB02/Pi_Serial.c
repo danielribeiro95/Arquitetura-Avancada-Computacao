6717cae435c5ffed3f9afb1d7c7a758bb735ea84
danielribeiro95 <ribeiro946@gmail.com>
2021-05-24 14:48:32 -0300

    Final

--- LAB02/Pi_Serial.c
+++ LAB02/Pi_Serial.c
@@ -5,8 +5,7 @@
 
 
 int main(){
-clock_t t;
-t = clock();
+
 
 float N = 1000000000;
 double dx = 1/N;
@@ -16,8 +15,6 @@ for (int i = 0; i <N ;i++){
     sum = sum + (4/(1+pow(i*dx,2)))*dx;
 }
 
-t = clock() - t;
-double tempo = ((double)t)/CLOCKS_PER_SEC;
 
-printf("pi = %f\n Tempo de Execucao: %f \n",sum,tempo);
+printf("pi = %f\n",sum);
 }
